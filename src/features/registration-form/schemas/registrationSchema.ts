3b9acb9d9075919e7a31942419390d961451094a
Danilo Valério <danvalerio89@gmail.com>
2026-07-07 13:32:39 -0300

    chore: change registration schema

--- src/features/registration-form/schemas/registrationSchema.ts
+++ src/features/registration-form/schemas/registrationSchema.ts
@@ -2,15 +2,16 @@ import { z } from "zod";
 
 export const registrationSchema = z
   .object({
-    name: z.string().min(1, "Nome é obrigatório"),
-    lastName: z.string().min(1, "Sobrenome é obrigatório"),
+    name: z.string().min(5, "Nome é obrigatório"),
+    lastName: z.string().min(5, "Sobrenome é obrigatório"),
     email: z.email("E-mail inválido"),
     confirmEmail: z.email("E-mail inválido"),
     country: z.string().min(1, "Selecione um país"),
     dateOfBirth: z.date({ error: "Data de nascimento é obrigatório" }),
-    acceptTerms: z.literal(true, {
-      error: "Você precisa aceitar os termos",
-    }),
+    // acceptTerms: z.literal(true, {
+    //   error: "Você precisa aceitar os termos",
+    // }),
+    acceptTerms: z.boolean().refine((v) => v === true),
   })
   .refine((data) => data.email === data.confirmEmail, {
     message: "Os e-mails não coincidem",
