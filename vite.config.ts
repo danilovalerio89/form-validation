f32f3a8a8975af172852dabd8099cb0f2c7626d8
Danilo Valério <danvalerio89@gmail.com>
2026-07-07 12:15:51 -0300

    chore: install chakra ui

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import babel from "@rolldown/plugin-babel";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    tsconfigPaths: true,
+  },
   server: {
     port: 3000,
   },
