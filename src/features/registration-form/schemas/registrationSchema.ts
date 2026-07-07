eef65b1fcd6716f80841aa329986fdf82a165155
Danilo Valério <danvalerio89@gmail.com>
2026-07-07 13:00:11 -0300

    chore: add registration schema

---
+++ src/features/registration-form/schemas/registrationSchema.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const registrationSchema = z
+  .object({
+    name: z.string().min(1, "Nome é obrigatório"),
+    lastName: z.string().min(1, "Sobrenome é obrigatório"),
+    email: z.email("E-mail inválido"),
+    confirmEmail: z.email("E-mail inválido"),
+    country: z.string().min(1, "Selecione um país"),
+    dateOfBirth: z.date({ error: "Data de nascimento é obrigatório" }),
+    acceptTerms: z.literal(true, {
+      error: "Você precisa aceitar os termos",
+    }),
+  })
+  .refine((data) => data.email === data.confirmEmail, {
+    message: "Os e-mails não coincidem",
+    path: ["confirmEmail"],
+  });
+
+export type RegistrationFormData = z.infer<typeof registrationSchema>;
