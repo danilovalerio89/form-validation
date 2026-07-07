e8c6442e1c89608e9427d1ba9ce874ecd06ac429
Danilo Valério <danvalerio89@gmail.com>
2026-07-07 13:36:33 -0300

    chore: use form for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { RegistrationForm } from "@/features/registration-form/components/RegistrationForm";
+import { Box, Center, Heading, Fieldset, Stack } from "@chakra-ui/react";
+
 function App() {
-  return <h1>Start</h1>;
+  return (
+    <Box>
+      <Center>
+        <Heading>Form Validation</Heading>
+      </Center>
+      <Box>
+        <Fieldset.Root size="lg" maxW="md">
+          <Stack>
+            <Fieldset.Legend>Form</Fieldset.Legend>
+            <Fieldset.HelperText>Formulário para teste</Fieldset.HelperText>
+          </Stack>
+
+          <RegistrationForm />
+        </Fieldset.Root>
+      </Box>
+    </Box>
+  );
 }
 
 export default App;
